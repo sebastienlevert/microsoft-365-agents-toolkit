7bdd567372a6c6ac06f7c397cc0d537cc65e1029
HuihuiWu-Microsoft <73154171+HuihuiWu-Microsoft@users.noreply.github.com>
2025-12-25 10:47:38 +0800

    feat: enable embedded knowledge (#14992)

--- packages/fx-core/src/common/featureFlags.ts
+++ packages/fx-core/src/common/featureFlags.ts
@@ -95,7 +95,7 @@ export class FeatureFlags {
   };
   static readonly EmbeddedKnowledgeEnabled = {
     name: FeatureFlagName.EmbeddedKnowledgeEnabled,
-    defaultValue: "false",
+    defaultValue: "true",
   };
   static readonly SandBoxedTeam = {
     name: FeatureFlagName.SandBoxedTeam,
